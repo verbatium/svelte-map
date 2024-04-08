e94e5ee18f86746d6b5daa36678050afaa76ec3d
Valeri Kuzmin <valeri@codeborne.com>
2024-04-08 12:04:30 +0300

    zoom in zoom out by mouse

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -4,10 +4,18 @@
   import {zoom} from './Zoom'
   import TileLayer from './TileLayer.svelte'
   import {afterUpdate} from 'svelte'
+  import {lest97} from '$lib/lamber'
+  import {limitNumberToRange} from '$lib/graphics'
 
   let svgElement: SVGElement
+  let zoomSpeed = 0.05
   let zoomLevel = 3
-  let viewBox: DOMRect = {x: 365000, y: 6375000, width: 740000 - 365000, height: 6635000 - 6375000,}
+  let z = 3
+  $: z = Math.ceil(zoomLevel)
+  $: changeViewBox(zoomLevel)
+  let maxWidth = 740000 - 365000
+  let maxHeight = 6635000 - 6375000
+  let viewBox: DOMRect = {x: 365000, y: 6375000, width: maxWidth, height: maxHeight}
   let width: number = 0
   let height: number = 0
 
@@ -17,30 +25,37 @@
   })
 
   let cursor: DOMPoint
+  let latLon: number[]
+  $: latLon = lest97.inverseConversion(cursor?.x ?? 0, cursor?.y ?? 0)
 
   function zoomIn() {
+    zoomLevel = limitNumberToRange(zoomLevel + zoomSpeed, 0, 14)
     console.log('zoomIn')
   }
 
   function zoomOut() {
+    zoomLevel = limitNumberToRange(zoomLevel - zoomSpeed, 0, 14)
     console.log('zoomOut')
   }
 
   function doZoom(value: number) {
-    let newZoomLevel = Math.max(Math.min(zoomLevel + value, 14), 0)
-    if (zoomLevel ! - newZoomLevel) {
-      zoomLevel = newZoomLevel
-      const centerX = viewBox.x + viewBox.width / 2
-      const centerY = viewBox.y + viewBox.height / 2
-      const newWidth = value > 0 ? viewBox.width / 2 : viewBox.width * 2
-      const newHeight = value > 0 ? viewBox.height / 2 : viewBox.height * 2
-      viewBox = {
-        x: centerX - newWidth / 2,
-        y: centerY - newHeight / 2,
-        width: newWidth,
-        height: newHeight,
-      }
+    zoomLevel = limitNumberToRange(zoomLevel + value, 0, 14)
+  }
+
+  function changeViewBox(zoomLevel: number) {
+    var k = Math.pow(2, zoomLevel-3)
+
+    const centerX = viewBox.x + viewBox.width / 2
+    const centerY = viewBox.y + viewBox.height / 2
+    const newWidth = maxWidth / k
+    const newHeight = maxHeight / k
+    viewBox = {
+      x: centerX - newWidth / 2,
+      y: centerY - newHeight / 2,
+      width: newWidth,
+      height: newHeight,
     }
+
   }
 
   function pan(event: CustomEvent<PanEventDetail>) {
@@ -49,7 +64,8 @@
 </script>
 <button class="border-amber-600 border-2 m-3 p-3" onclick={()=>doZoom(-1)}>-</button>
 <button class="border-amber-600 border-2  m-3 p-3" onclick={()=>doZoom(+1)}>+</button>
-<span class="ml-2">Zoom: {zoomLevel} Location: {Math.round(cursor?.x)}, {Math.round(cursor?.y)}</span>
+<span class="ml-2">Zoom: {zoomLevel} Location: {Math.round(cursor?.x)}, {Math.round(cursor?.y)} ({latLon[0]}
+  , {latLon[1]}) {z}</span>
 <svg
   {...$$restProps}
   bind:this={svgElement}
@@ -64,7 +80,7 @@
   xmlns="http://www.w3.org/2000/svg"
 >
   <g transform="matrix(1 0 0 -1 0 {2 * viewBox.y + viewBox.height})">
-    <TileLayer tileMapUrl="https://tiles.maaamet.ee/tm/tms/1.0.0/foto@LEST" zoomLevel={zoomLevel} viewBox={viewBox}/>
-    <TileLayer tileMapUrl="https://tiles.maaamet.ee/tm/tms/1.0.0/hybriid@LEST" zoomLevel={zoomLevel} viewBox={viewBox}/>
+    <TileLayer tileMapUrl="https://tiles.maaamet.ee/tm/tms/1.0.0/foto@LEST" viewBox={viewBox} zoomLevel={z}/>
+    <TileLayer tileMapUrl="https://tiles.maaamet.ee/tm/tms/1.0.0/hybriid@LEST" viewBox={viewBox} zoomLevel={z}/>
   </g>
 </svg>
