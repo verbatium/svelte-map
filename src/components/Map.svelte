13faf00906f3eaa3a3047b0c9db818848fdf9a2e
Valeri Kuzmin <valeri@codeborne.com>
2024-04-14 18:13:32 +0300

    handle map resize

--- src/components/Map.svelte
+++ src/components/Map.svelte
@@ -1,75 +1,19 @@
 <svelte:options namespace="svg"/>
 <script lang="ts">
-  import type {PanEventDetail} from './Zoom'
-  import {zoom} from './Zoom'
-  import {afterUpdate} from 'svelte'
-  import {lest97} from '$lib/lamber'
-  import {limitNumberToRange} from '$lib/graphics'
+  import SvgCartesian from './svg/SvgCartesian.svelte'
 
-  let svgElement: SVGElement
-  let zoomSpeed = 0.05
-  let zoomLevel = 4
-  let z = 4
-  $: z = Math.floor(zoomLevel)
-  $: changeViewBox(zoomLevel)
-  let maxWidth = 740000 - 365000
-  let maxHeight = 6635000 - 6375000
-  let viewBox: DOMRect = {x: 365000, y: 6375000, width: maxWidth, height: maxHeight}
-  let width: number = 0
-  let height: number = 0
+  let width: number
+  let height: number
+  let initialViewBox: DOMRect = {x: 365000, y: 6375000, width: 740000 - 365000, height: 6635000 - 6375000}
+  let viewBox = initialViewBox
+  let zoomLevel = 0
+  $: unitsPerPixel = (width && height && viewBox) ? Math.max(viewBox.width / width, viewBox.height / height) : 4000
+  $: zoomLevel = Math.max(Math.min(Math.round(Math.log2(4000 / unitsPerPixel, 2)), 14), 0)
 
-  afterUpdate(() => {
-    width = svgElement?.clientWidth
-    height = svgElement?.clientHeight
-  })
-
-  let cursor: DOMPoint
-  let latLon: number[]
-  $: latLon = lest97.inverseConversion(cursor?.x ?? 0, cursor?.y ?? 0)
-
-  function zoomIn() {
-    zoomLevel = limitNumberToRange(zoomLevel + zoomSpeed, 0, 14.95)
-  }
-
-  function zoomOut() {
-    zoomLevel = limitNumberToRange(zoomLevel - zoomSpeed, 0, 14.95)
-  }
-
-  function changeViewBox(zoomLevel: number) {
-    var k = Math.pow(2, zoomLevel - 4)
-
-    const centerX = viewBox.x + viewBox.width / 2
-    const centerY = viewBox.y + viewBox.height / 2
-    const newWidth = maxWidth / k
-    const newHeight = maxHeight / k
-    viewBox = {
-      x: centerX - newWidth / 2,
-      y: centerY - newHeight / 2,
-      width: newWidth,
-      height: newHeight,
-    }
-
-  }
-
-  function pan(event: CustomEvent<PanEventDetail>) {
-    viewBox = {...viewBox, x: viewBox.x - event.detail.deltaX, y: viewBox.y - event.detail.deltaY}
-  }
 </script>
-
-<svg
-  {...$$restProps}
-  bind:this={svgElement}
-  class="w-full touch-none {$$props.class ?? ''}"
-  on:cursor={(e)=>{cursor = e.detail}}
-  on:pan={pan}
-  on:zoomIn={zoomIn}
-  on:zoomOut={zoomOut}
-  role="presentation"
-  use:zoom={{zoomLevel, width, height}}
-  viewBox="{viewBox.x} {viewBox.y} {viewBox.width} {viewBox.height}"
-  xmlns="http://www.w3.org/2000/svg"
->
-  <g transform="matrix(1 0 0 -1 0 {2 * viewBox.y + viewBox.height})">
-    <slot {viewBox} zoomLevel={z}/>
-  </g>
-</svg>
+<SvgCartesian bind:height bind:width
+              on:viewboxchanged={(e:CustomEvent<DOMRect>)=>viewBox=e.detail}
+              on:cursor
+              viewBox={initialViewBox}>
+  <slot {viewBox} {zoomLevel}/>
+</SvgCartesian>
--- src/components/svg/SvgCartesian.svelte
+++ src/components/svg/SvgCartesian.svelte
@@ -2,6 +2,8 @@
   import {panZoom} from '../PanZoom'
 
   export let viewBox: DOMRect
+  export let width: number = 100
+  export let height: number = 100
   let cartesianMatrix = [1, 0, 0, -1, -viewBox.x, viewBox.height + viewBox.y]
   let viewBoxString = `0,0,${viewBox.width},${viewBox.height}`
   let transform = `matrix(${cartesianMatrix.join(',')})`
@@ -15,9 +17,16 @@
     svg.viewBox.baseVal.y = 0
     svg.viewBox.baseVal.width = viewBox.width
     svg.viewBox.baseVal.height = viewBox.height
+    resize()
+  }
+
+  function resize() {
+    width = svg?.clientWidth ?? 100
+    height = svg?.clientHeight ?? 100
   }
 
 </script>
+<svelte:window on:resize={resize}/>
 <svg {...$$restProps}
      bind:this={svg}
      class="border border-amber-600 m-20 bg-amber-200"
@@ -30,5 +39,4 @@
   <g transform={transform}>
     <slot/>
   </g>
-
 </svg>
