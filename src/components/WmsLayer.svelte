b16475b254bb779e6b74e83e43b55632d0b3542a
Valeri Kuzmin <valeri@codeborne.com>
2024-04-24 21:25:22 +0300

    display wms map by tiles

--- src/components/WmsLayer.svelte
+++ src/components/WmsLayer.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import {debounce} from '$lib/debounce'
-  import type {Point} from '$lib/graphics'
+  import {epsg3301Tiles} from '$lib/tms/Epsg3301Tiles'
 
   export let baseUrl = 'https://gis.vta.ee/primar/wms_ip/TranspordiametNutimeri'
   export let srs = 'EPSG:3301'
@@ -9,17 +9,22 @@
   export let mapFormat = 'image/png'
   export let transparent = true
 
-  function getUrl(viewBox: DOMRect): string {
+  function getUrl(viewBox: {
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  }, width: number, height: number): string {
     let url = new URL(baseUrl)
     url.searchParams.set('REQUEST', 'GetMap')
     url.searchParams.set('SERVICE', 'WMS')
     url.searchParams.set('VERSION', '1.1.1')
-    url.searchParams.set('FORMAT',mapFormat )
+    url.searchParams.set('FORMAT', mapFormat)
     url.searchParams.set('STYLES', styles)
-    url.searchParams.set('TRANSPARENT', transparent)
+    url.searchParams.set('TRANSPARENT', '' + transparent)
     url.searchParams.set('LAYERS', layers)
-    url.searchParams.set('WIDTH', g.ownerSVGElement.clientWidth)
-    url.searchParams.set('HEIGHT', g.ownerSVGElement.clientHeight)
+    url.searchParams.set('WIDTH', '' + width)
+    url.searchParams.set('HEIGHT', '' + height)
     url.searchParams.set('SRS', srs)
     const bbox = `${viewBox.x},${viewBox.y},${viewBox.x + viewBox.width},${viewBox.y + viewBox.height}`
     url.searchParams.set('BBOX', bbox)
@@ -28,54 +33,48 @@
 
   export let zoomLevel: number
   export let viewBox: DOMRect
-  let g: SVGElement
 
 
-  let visibleTiles: Map<number, Point[]> = new Map()
-  let tileLayers: number[] = []
-  let tilesPerAxis: number
+  function getTiles(viewBox: DOMRect): { href: string; x: number; y: number, width: number, height: number } [] {
+    console.log('getTiles', viewBox )
+    return epsg3301Tiles.visibleTilesByClientViewBox(viewBox, zoomLevel)
+      .map(([x, y]) => {
+          let bboxByTileXY = epsg3301Tiles.bboxByTileXY(x, y, zoomLevel)
+          return {
+            x: bboxByTileXY.x,
+            y: bboxByTileXY.y,
+            width: bboxByTileXY.width,
+            height: bboxByTileXY.height,
+            href: getUrl(bboxByTileXY, epsg3301Tiles.tileSize, epsg3301Tiles.tileSize),
+          }
+        },
+      )
+  }
+
+  let visibleTiles: Map<number, { href: string; x: number; y: number, width: number, height: number }[]> = new Map()
+  let zoomLevels: number[] = []
 
   const debouncedTileCalculator = debounce(calculateTiles, 20)
-  $: g && viewBox && debouncedTileCalculator(viewBox, zoomLevel)
+  $: debouncedTileCalculator(viewBox, zoomLevel)
 
   function calculateTiles(viewBox: DOMRect, zoomLevel: number) {
-    tilesPerAxis = Math.pow(2, zoomLevel)
-    visibleTiles.set(zoomLevel, [...visibleTiles.get(zoomLevel) ?? [], ...getTiles(tilesPerAxis, viewBox)])
-    visibleTiles = new Map(visibleTiles)
-    tileLayers = [zoomLevel]
-  }
-
-  function getTiles(tilesPerAxis: number, viewBox: DOMRect): Point [] {
-    let screenToRealMatrix = getScreenToRealMatrix(viewBox)
-    let boundingClientRect = g.ownerSVGElement.getBoundingClientRect()
-    let domPoint = screenToRealMatrix.transformPoint(new DOMPoint(boundingClientRect.x, boundingClientRect.y))
-    let domPoint2 = screenToRealMatrix.transformPoint(new DOMPoint(boundingClientRect.x + boundingClientRect.width,
-      boundingClientRect.y + boundingClientRect.height))
-    let element = {
-      x: domPoint.x,
-      y: domPoint.y,
-      width: domPoint2.x - domPoint.x,
-      height: domPoint2.y - domPoint.y,
-      href: getUrl(new DOMRect(domPoint.x, domPoint.y, domPoint2.x - domPoint.x, domPoint2.y - domPoint.y)),
+    console.log('calculateTiles')
+    visibleTiles.set(zoomLevel, getTiles(viewBox))
+    if (transparent) {
+      zoomLevels = [zoomLevel]
+    } else {
+      zoomLevels = Array.from(visibleTiles.keys()).filter(l => l <= zoomLevel).sort((a, b) => a - b)
     }
-    return [element]
-  }
-
-  function getScreenToRealMatrix(viewBox: DOMRect) {
-    return (DOMMatrix.fromMatrix(g.getScreenCTM() as DOMMatrix))
-      .scale(1, -1)
-      .translate(0, -((2 * viewBox.y + viewBox.height)))
-      .inverse()
   }
 </script>
 
 
-<g bind:this={g}>
-  {#each tileLayers as level (level)}
-    {#each visibleTiles.get(level) as tile (tile.href)}
-      <g transform="matrix(1 0 0 -1 0 {2 * tile.y + tile.height})">
-        <image x="{tile.x}" y="{tile.y}" href={tile.href} width="{tile.width}" height="{tile.height}"/>
-      </g>
-    {/each}
+{#each zoomLevels as level}
+  {#each visibleTiles?.get(level) ?? [] as tile (tile.href)}
+    <g transform="translate({tile.x}, {tile.y}) scale(1,-1) translate(0,{-tile.height})">
+      <image href={tile.href} width={tile.width} height={tile.height}/>
+      <rect width={tile.width} height={tile.height} stroke="black" stroke-width="1" vector-effect="non-scaling-stroke"
+            fill="none"/>
+    </g>
   {/each}
-</g>
+{/each}
