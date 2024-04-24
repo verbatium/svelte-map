75f3cec86b2b5eb93ebc7fd5cb08ff4536552866
Valeri Kuzmin <valeri@codeborne.com>
2024-04-24 17:37:28 +0300

    calculate Epsg3301 tiles for WMS

--- src/lib/tms/Epsg3301Tiles.test.ts
+++ src/lib/tms/Epsg3301Tiles.test.ts
@@ -10,12 +10,50 @@ describe('Epsg3301Tiles', () => {
     expect(epsg3301Tiles.unitsPerPixel(14)).eql(0.244140625)
   })
   
-  it('unitsPerPixel at specified level ', () => {
-    expect(epsg3301Tiles.bbox(40500, 5993000, 0)).eql({x: 40500, y: 5993000, width: 1024000, height: 1024000})
-    expect(epsg3301Tiles.bbox(40500, 5993000, 1)).eql({x: 40500, y: 5993000, width: 512000, height: 512000})
-    expect(epsg3301Tiles.bbox(40500+512000, 5993000, 1)).eql({x: 40500+512000, y: 5993000, width: 512000, height: 512000})
-    expect(epsg3301Tiles.bbox(40500+512000-1, 5993000, 1)).eql({x: 40500, y: 5993000, width: 512000, height: 512000})
-    expect(epsg3301Tiles.bbox(40500, 5993000+512000, 1)).eql({x: 40500, y: 5993000+512000, width: 512000, height: 512000})
-    expect(epsg3301Tiles.bbox(40500, 5993000+512000-1, 1)).eql({x: 40500, y: 5993000, width: 512000, height: 512000})
+  it('bboxByUserXY', () => {
+    expect(epsg3301Tiles.bboxByUserXY(40500, 5993000, 0)).eql({x: 40500, y: 5993000, width: 1024000, height: 1024000})
+    expect(epsg3301Tiles.bboxByUserXY(40500, 5993000, 1)).eql({x: 40500, y: 5993000, width: 512000, height: 512000})
+    expect(epsg3301Tiles.bboxByUserXY(40500 + 512000, 5993000, 1)).eql({
+      x: 40500 + 512000,
+      y: 5993000,
+      width: 512000,
+      height: 512000,
+    })
+    expect(epsg3301Tiles.bboxByUserXY(40500 + 512000 - 1, 5993000, 1)).eql({
+      x: 40500,
+      y: 5993000,
+      width: 512000,
+      height: 512000,
+    })
+    expect(epsg3301Tiles.bboxByUserXY(40500, 5993000 + 512000, 1)).eql({
+      x: 40500,
+      y: 5993000 + 512000,
+      width: 512000,
+      height: 512000,
+    })
+    expect(epsg3301Tiles.bboxByUserXY(40500, 5993000 + 512000 - 1, 1)).eql({
+      x: 40500,
+      y: 5993000,
+      width: 512000,
+      height: 512000,
+    })
+  })
+  
+  it('bboxByTileXY ', () => {
+    expect(epsg3301Tiles.bboxByTileXY(0, 0, 0)).eql({x: 40500, y: 5993000, width: 1024000, height: 1024000})
+    expect(epsg3301Tiles.bboxByTileXY(0, 0, 1)).eql({x: 40500, y: 5993000, width: 512000, height: 512000})
+    expect(epsg3301Tiles.bboxByTileXY(1, 0, 1)).eql({x: 40500 + 512000, y: 5993000, width: 512000, height: 512000})
+    expect(epsg3301Tiles.bboxByTileXY(0, 1, 1)).eql({x: 40500, y: 5993000 + 512000, width: 512000, height: 512000})
+  })
+  
+  it('bboxByTileXY ', () => {
+    expect(epsg3301Tiles.userXYToTileXY(40500, 5993000, 0)).eql([0, 0, 0])
+    expect(epsg3301Tiles.userXYToTileXY( 40500, 5993000 + 512000, 1)).eql([0, 1, 1])
+  })
+  
+  it('neighborsInSpiral ', () => {
+    expect(epsg3301Tiles.neighborsInSpiral(5,5,0)).eql([[5,5]])
+    expect(epsg3301Tiles.neighborsInSpiral(5,5,1)).eql([[5,5], [5,6], [6,6], [6,5]])
+    expect(epsg3301Tiles.neighborsInSpiral(5,5,2)).eql([[5,5], [5,6], [6,6], [6,5], [6,4], [5,4], [4,4], [4,5], [4,6]])
   })
 })
--- src/lib/tms/Epsg3301Tiles.ts
+++ src/lib/tms/Epsg3301Tiles.ts
@@ -6,22 +6,29 @@ export class Epsg3301Tiles {
   originX = 40500.000000
   originY = 5993000.000000
   srs = 'EPSG:3301'
-  tileWidth = 256
-  tileHeight = 256
+  tileSize = 256
   mimeType = 'image/png'
   extension = 'png'
   unitsPerPixel0 = 4000
   maxLevel = 14
   
-  bbox(x: number, y: number, z: number): { x: number, y: number, width: number, height: number } {
-    const perPixel = this.unitsPerPixel(z)
-    const width = this.tileWidth * perPixel
-    const height = this.tileHeight * perPixel
+  bboxByUserXY(x: number, y: number, z: number): { x: number, y: number, width: number, height: number } {
+    const size = this.tileSize * this.unitsPerPixel(z)
     return {
-      x: this.getStartOfTile(this.originX, x, width),
-      y: this.getStartOfTile(this.originY, y, height),
-      width: width,
-      height: height,
+      x: this.getStartOfTile(this.originX, x, size),
+      y: this.getStartOfTile(this.originY, y, size),
+      width: size,
+      height: size,
+    }
+  }
+  
+  bboxByTileXY(x: number, y: number, z: number): { x: number, y: number, width: number, height: number } {
+    const size = this.tileSize * this.unitsPerPixel(z)
+    return {
+      x: this.originX + x * size,
+      y: this.originY + y * size,
+      width: size,
+      height: size,
     }
   }
   
@@ -31,5 +38,58 @@ export class Epsg3301Tiles {
   
   unitsPerPixel(z: number): number {
     return this.unitsPerPixel0 / (1 << z)
+  }
+  
+  
+  userXYToTileXY(userX: number, userY: number, z: number): number[] {
+    const perPixel = this.unitsPerPixel(z)
+    const tileX = Math.floor((userX - this.originX) / perPixel / this.tileSize)
+    const tileY = Math.floor((userY - this.originY) / perPixel / this.tileSize)
+    return [tileX, tileY, z]
+  }
+  
+  visibleTilesByClientViewBox(viewBox: { x: number, y: number, width: number, height: number }, z: number): number[][] {
+    const p1 = this.userXYToTileXY(viewBox.x, viewBox.y, z)
+    const p2 = this.userXYToTileXY(viewBox.x + viewBox.width, viewBox.y + viewBox.height, z)
+    
+    const diameter = Math.max(p2[0] - p1[0], p2[1] - p1[1])
+    const radius = Math.floor(diameter / 2)
+    const center = p1.map(i => i + radius)
+    return this.neighborsInSpiral(center[0], center[1], diameter)
+  }
+  
+  neighborsInSpiral(x: number, y: number, diameter: number): number[][] {
+    let n = 0
+    let x1 = x
+    let y1 = y
+    let result: number[][] = [[x, y]]
+    while (n < diameter) {
+      n++
+      if (n % 2 == 1)//if N is odd:
+      {
+        y1++ //move up 1 step
+        result.push([x1, y1])
+        for (let i = 0; i < n; i++) { // move down N steps
+          x1++
+          result.push([x1, y1])
+        }
+        for (let i = 0; i < n; i++) { //      move left N steps
+          y1--
+          result.push([x1, y1])
+        }
+      } else {//if N is odd:
+        y1-- //move down 1 step
+        result.push([x1, y1])
+        for (let i = 0; i < n; i++) { // move up N steps
+          x1--
+          result.push([x1, y1])
+        }
+        for (let i = 0; i < n; i++) { //move right N steps
+          y1++
+          result.push([x1, y1])
+        }
+      }
+    }
+    return result
   }
 }
